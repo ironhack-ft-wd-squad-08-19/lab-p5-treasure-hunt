3b9048ea46c4d808fc9acb4e4f27ecf0d6e37b40
Nelly-Charlott Schneider <nelly@Nelly-Charlotts-MacBook-Pro.local>
2019-08-22 17:52:59 +0200

    done

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 const game = new Game();
+const player = new Player();
+const treasure = new Treasure();
+// const treasure = new Treasure(
+//   Math.floor(Math.random() * 10),
+//   Math.floor(Math.random() * 10)
+// );
+
+let viDown, viUp, viLeft, viRight, crystal;
+function preload() {
+  viDown = loadImage("../assets/character-down.png");
+  viUp = loadImage("../assets/character-up.png");
+  viLeft = loadImage("../assets/character-left.png");
+  viRight = loadImage("../assets/character-right.png");
+  crystal = loadImage("../assets/treasure.png");
+  treasure.setRandomPosition();
+}
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
@@ -7,4 +23,28 @@ function setup() {
 
 function draw() {
   game.drawGrid();
+  player.draw();
+  treasure.draw();
+  // if (keyIsPressed) {
+  //   keyPressed();
+  // }
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    console.log("left");
+    player.moveLeft();
+  }
+  if (keyCode === RIGHT_ARROW) {
+    console.log("right");
+    player.moveRight();
+  }
+  if (keyCode === UP_ARROW) {
+    console.log("left");
+    player.moveUp();
+  }
+  if (keyCode === DOWN_ARROW) {
+    console.log("right");
+    player.moveDown();
+  }
 }
